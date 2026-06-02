87af2ebb062c39d16ccb59ddd17efb9bf45caf0b
MayconSantos04 <gadsence47@gmail.com>
2026-06-02 13:05:44 -0300

    feat: added some informations and conditions to search books for the id each book.

--- src/controllers/book.controllers.js
+++ src/controllers/book.controllers.js
@@ -7,6 +7,14 @@ export class BookController {
     res.send(books);
   }
 
+  async listId(req, res) {
+    const resultId = this.datadb.find((datadbs) => {
+      return datadbs.id === req.params.id;
+    });
+
+    res.send(resultId);
+  }
+
   async create(req, res) {
     const { titulo, autor, genero, disponivel } = req.body;
     const randomUUID = crypto.randomUUID();
--- src/routers/book.routes.js
+++ src/routers/book.routes.js
@@ -5,6 +5,7 @@ export const bookRouter = Router();
 export const bookController = new BookController();
 
 bookRouter.get("/books", (req, res) => bookController.list(req, res));
+bookRouter.get("/books/:id", (req, res) => bookController.listId(req, res));
 bookRouter.post("/books", (req, res) => bookController.create(req, res));
 bookRouter.patch("/books/:id", (req, res) => bookController.updateBook(req, res));
 bookRouter.put("/books/:id", (req, res) => bookController.update(req, res));
