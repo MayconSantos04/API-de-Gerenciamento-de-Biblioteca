d360af1fddc6a323a1d5329595591c493ec443cf
MayconSantos04 <gadsence47@gmail.com>
2026-06-02 12:19:06 -0300

    feat: crated new two methods, GET and POST.

--- src/controllers/book.controllers.js
+++ src/controllers/book.controllers.js
@@ -1,1 +1,27 @@
-export class BookController {};
+import crypto from "node:crypto";
+export class BookController {
+  datadb = [];
+
+  async list(req, res) {
+    const books = await this.datadb;
+    res.send(books);
+  }
+
+  async create(req, res) {
+    const { titulo, autor, genero, disponivel } = req.body;
+    const randomUUID = crypto.randomUUID();
+
+    const book = {
+      id: randomUUID,
+      titulo: titulo,
+      autor: autor,
+      genero: genero,
+      disponivel: disponivel,
+    };
+
+    await this.datadb.push(book);
+
+    console.log(this.datadb);
+    return res.status(201).json(book);
+  }
+}
--- src/routers/book.routes.js
+++ src/routers/book.routes.js
@@ -4,7 +4,8 @@ import { BookController } from "../controllers/book.controllers.js";
 export const bookRouter = Router();
 export const bookController = new BookController();
 
-bookRouter.get("/books", (req, res) => BookController.list(req, res));
-bookRouter.post("/books", (req, res) => BookController.create(req, res));
-bookRouter.patch("/books/:id", (req, res) => BookController.update(req, res));
-bookRouter.delete("/books/:id", (req, res) => BookController.remove(req, res));
+bookRouter.get("/books", (req, res) => bookController.list(req, res));
+bookRouter.post("/books", (req, res) => bookController.create(req, res));
+bookRouter.patch("/books/:id", (req, res) => bookController.updateBook(req, res));
+bookRouter.put("/books/:id", (req, res) => bookController.update(req, res));
+bookRouter.delete("/books/:id", (req, res) => bookController.remove(req, res));
