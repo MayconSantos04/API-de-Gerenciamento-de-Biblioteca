05d2f004450f85116451c2bfa75b618f06988d33
MayconSantos04 <gadsence47@gmail.com>
2026-07-01 19:46:39 -0300

    feat: resolution erro of cors.

--- src/server.js
+++ src/server.js
@@ -3,15 +3,18 @@ import { bookRouter } from "./routers/book.routes.js";
 import { loanRouter } from "./routers/emprestimos.routes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "../swagger.js";
+import cors from "cors";
+
 
 const app = express();
 const PORT = 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use(bookRouter);
 app.use(loanRouter);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(PORT, () => {
-  console.log(`Port open at ${PORT}`);
+  console.log(`Port open im ${PORT}`);
 });
